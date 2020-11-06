631ddee56026da3ee7ff4e2be626c02c0ba167c5
singamsricharan <singam.sricharan1603@gmail.com>
2020-11-06 13:28:15 +0530

    chages

--- MatterJSBoilerPlate-master/sketch.js
+++ MatterJSBoilerPlate-master/sketch.js
@@ -16,9 +16,16 @@ function setup() {
     Engine.run(engine);
     mango1=new Mango(1100,200,10);
     mango2=new Mango(800,200,10);
-    mango3=new Mango(900,150,10);
-    mango4=new Mango(1000,150,10);
+    mango3=new Mango(900,100,10);
+    mango4=new Mango(1000,100,10);
     mango5=new Mango(700,250,10);
+    mango6=new Mango(1100,230,10);
+    mango7=new Mango(850,230,10);
+    mango8=new Mango(800,150,10);
+    mango9=new Mango(1000,200,10);
+    mango10=new Mango(650,240,10);
+    mango11=new Mango(1000,100,10);
+    mango12=new Mango(1100,150,10);
 	stone1=new Stone(260,430,10);
 	chain1=new chain(stone1.body,{x:260,y:430});
     tree1=new Tree(600,10,100,100);
@@ -35,6 +42,18 @@ function draw() {
   mango3.display();
   mango4.display();
   mango5.display();
+  mango6.display();
+  mango7.display();
+  mango8.display();
+  mango9.display();
+  mango10.display();
+  mango11.display();
+  mango12.display();
+  dectectcollision(stone1,mango1);
+  dectectcollision(stone1,mango2);
+  dectectcollision(stone1,mango3);
+  dectectcollision(stone1,mango4);
+  dectectcollision(stone1,mango5);
   ground1.display();
   image(boyImage,200,350,300,300);
   drawSprites();
@@ -50,4 +69,12 @@ function keyPressed(){
         Matter.Body.setPosition(stone1.body,{x:200,y:200})
         chain1.attach(stone1.body);
     }
+}
+function dectectcollision(lstone,lmango){
+mangoBodyPosition=lmango.body.position
+stoneBodyPosition=lstone.body.position
+var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+if(distance<=lmango.r+lstone.r){
+    Matter.Body.setStatic(lmango.body,false)
+}
 }
